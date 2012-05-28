6aeeff7ce4842a653ac33f06b3b37d0b8338e98c
Mark S. Petrovic <mspetrovic@gmail.com>
2012-05-28 10:35:43 -0700

    Add target/generated-sources to compiler source inputs so IDEA augments its project model.
    Add parser and lexer headers containing the intended Java package name.
    
    Add test harness to App so we can run the parser.

--- src/main/antlr/org/petrovic/antlr/T.g
+++ src/main/antlr/org/petrovic/antlr/T.g
@@ -1,5 +1,19 @@
 grammar T;
+
+@parser::header {
+package org.petrovic.antlr;
+}
+
+@members {
+int blah = 0;
+}
+
+@lexer::header {
+package org.petrovic.antlr;
+}
+
 /** Match things like "call foo;" */
 r : 'call' ID ';' {System.out.println("invoke "+$ID.text);} ;
+
 ID: 'a'..'z'+ ;
 WS: (' '|'\n'|'\r')+   {$channel=HIDDEN;} ; // ignore whitespace
--- src/main/java/org/petrovic/antlr/App.java
+++ src/main/java/org/petrovic/antlr/App.java
@@ -1,13 +1,19 @@
 package org.petrovic.antlr;
 
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("waiting for input...");
+
+        ANTLRInputStream antlrInputStream = new ANTLRInputStream(System.in);
+        TLexer tLexer = new TLexer(antlrInputStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream((tLexer));
+        TParser tParser = new TParser(commonTokenStream);
+        tParser.r();
     }
 }
